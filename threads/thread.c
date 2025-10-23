8ec74e56a08822137d7c9641fa9e7ae39c08e78f
hambeau014-design <hambeau014@gmail.com>
2025-10-23 14:38:31 +0900

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -345,7 +345,12 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority)
 {
-    thread_current ()->priority = new_priority;
+   struct thread *cur = thread_current();
+   int old_priority = cur->priority;
+   cur->priority = new_priority;
+
+   if(new_priority < old_priority)
+      thread_yield();
 }
 
 /* Returns the current thread's priority. */
