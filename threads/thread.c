e1f3f99004e78446e2cb3f2d67fad61075242c5e
hambeau014-design <hambeau014@gmail.com>
2025-10-23 14:29:29 +0900

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -247,7 +247,9 @@ thread_unblock (struct thread *t)
     ASSERT (t->status == THREAD_BLOCKED);
     list_push_back (&ready_list, &t->elem);
     t->status = THREAD_READY;
-    intr_set_level (old_level);
+    if(thread_current() != idle_thread && t->priority > thread_current()->priority)
+       thread_yield();
+   intr_set_level (old_level);
 }
 
 /* Returns the name of the running thread. */
@@ -585,3 +587,11 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+bool
+thread_cmp_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+{
+   struct thread *t_a = list_entry(a, struct thread, elem);
+   struct thread *t_b = list_entry(b, struct thread, elem);
+   return t_a->priority > t_b->priority;
+}
