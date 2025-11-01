44706496a1d167bfbbfaab6c6f8688e7b28ad0f6
hambeau014-design <hambeau014@gmail.com>
2025-11-01 17:14:49 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -140,19 +140,22 @@ sema_up (struct semaphore *sema)
 
     ASSERT (sema != NULL);
     old_level = intr_disable ();
+
+    /* 이 줄이 반드시 필요함 */
+    list_sort (&sema->waiters, sema_thread_priority_cmp, NULL);
+
     if (!list_empty (&sema->waiters)) {
-        struct thread *t = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
-        thread_unblock (t); /* thread_unblock checks preemption */
+        struct thread *t = list_entry (list_pop_front (&sema->waiters),
+                                       struct thread, elem);
+        thread_unblock (t);
     }
     sema->value++;
     intr_set_level (old_level);
 
-    /* 선점 고려: 깨운 스레드가 더 높은 우선순위면 양보 */
-    if (!intr_context()) {
+    if (!intr_context())
         thread_yield();
-    } else {
+    else
         intr_yield_on_return();
-    }
 }
 
 /* lock implementation with priority donation support */
