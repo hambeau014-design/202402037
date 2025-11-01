e14e807fe120bf4147a02e54ea603deef86842f3
hambeau014-design <hambeau014@gmail.com>
2025-11-01 18:08:20 +0900

    Update timer.h

--- devices/timer.h
+++ devices/timer.h
@@ -7,14 +7,15 @@
 void timer_init (void);
 void timer_calibrate (void);
 
-void timer_sleep (int64_t ticks);
-void timer_msleep (int64_t ms);
-void timer_usleep (int64_t us);
-void timer_nsleep (int64_t ns);
-void timer_mdelay (int64_t ms);
-void timer_udelay (int64_t us);
-void timer_ndelay (int64_t ns);
-int64_t timer_ticks (void);
-int64_t timer_elapsed (int64_t);
+void    timer_sleep (int64_t ticks);
+void    timer_msleep (int64_t ms);
+void    timer_usleep (int64_t us);
+void    timer_nsleep (int64_t ns);
+void    timer_mdelay (int64_t ms);
+void    timer_udelay (int64_t us);
+void    timer_ndelay (int64_t ns);
 
-#endif /* devices/timer.h */
+int64_t timer_ticks (void);
+int64_t timer_elapsed (int64_t then);
+
+#endif
