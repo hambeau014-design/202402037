830c8cb5f62df5b711d8ba59dc45b92751659869
hambeau014-design <hambeau014@gmail.com>
2025-11-01 18:06:22 +0900

    Update synch.h

--- threads/synch.h
+++ threads/synch.h
@@ -3,36 +3,40 @@
 
 #include <list.h>
 #include <stdbool.h>
+#include "threads/thread.h"
 
-/* A counting semaphore. */
+/* Counting semaphore. */
 struct semaphore {
-  unsigned value;            /* Current value. */
-  struct list waiters;       /* List of waiting threads (thread.elem). */
+  unsigned value;
+  struct list waiters;   /* of struct thread via thread.elem (priority/FIFO) */
 };
 
-/* Lock. */
+/* Lock with priority donation. */
 struct lock {
-  struct thread *holder;     /* Thread holding lock (for debugging). */
-  struct semaphore semaphore;/* Binary semaphore controlling access. */
+  struct thread *holder;
+  struct semaphore semaphore;  /* binary semaphore */
 };
 
 /* Condition variable. */
 struct condition {
-  struct list waiters;       /* list of struct semaphore_elem. */
+  struct list waiters;         /* of struct semaphore_elem (see synch.c) */
 };
 
+/* Semaphore API */
 void sema_init (struct semaphore *, unsigned value);
 void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
 void sema_up (struct semaphore *);
 void sema_self_test (void);
 
+/* Lock API */
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
+/* Condition variable API */
 void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
